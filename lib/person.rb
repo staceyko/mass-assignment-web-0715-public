116574300ddc1051980eaf71eed6a971a6c6a20b
staceyko <ko.stacey@gmail.com>
2015-08-12 14:30:37 -0400

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-  # code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(attributes)
+    attributes.each do |k, v|
+      instance_variable_set("@#{k}", v)
+        # self.class.send("#{k}=", v)
+    end
+  end
+
 end
